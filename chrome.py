cfca7a2989bd41c3a0828802f712a5b75bf89561
Gan <62144173+TechZhao@users.noreply.github.com>
2025-04-26 20:27:52 +0800

    Update chrome.py for python3

--- chrome.py
+++ chrome.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Get relevant history from the Google Chrome history database based on the given query and build
 Alfred items based on the results.
 
 Usage:
-    chrome [--no-favicons | --favicons] PROFILE QUERY
-    chrome (-h | --help)
-    chrome --version
+    chrome.py [--no-favicons | --favicons] PROFILE QUERY
+    chrome.py (-h | --help)
+    chrome.py --version
 
 The path to the Chrome user profile to get the history database from is given in PROFILE. The query
 to search for is given in QUERY. The output is formatted as the Alfred script filter XML output
@@ -32,7 +32,7 @@ import time
 import datetime
 from docopt import docopt
 
-__version__ = '0.8.0'
+__version__ = '0.7.0'
 
 CACHE_EXPIRY = 60
 HISTORY_DB = 'History'
@@ -50,6 +50,14 @@ FAVICON_SELECT = u"""
     , favicon_bitmaps.image_data, favicon_bitmaps.last_updated
 """
 
+HISTORY_QUERY = u"""
+SELECT urls.id, urls.title, urls.url {favicon_select}
+    FROM urls
+    {favicon_join}
+    WHERE (urls.title LIKE ? OR urls.url LIKE ?)
+    ORDER BY visit_count DESC, typed_count DESC, last_visit_time DESC
+"""
+
 UNIX_EPOCH = datetime.datetime.utcfromtimestamp(0)
 WINDOWS_EPOCH = datetime.datetime(1601, 1, 1)
 SECONDS_BETWEEN_UNIX_AND_WINDOWS_EPOCH = (UNIX_EPOCH - WINDOWS_EPOCH).total_seconds()
@@ -57,7 +65,7 @@ MICROSECS_PER_SEC = 10 ** -6
 
 class ErrorItem(alfred.Item):
     def __init__(self, error):
-        alfred.Item.__init__(self, {u'valid': u'NO', u'autocomplete': error.message}, error.message, u'Check the workflow log for more information.')
+        alfred.Item.__init__(self, {'valid': 'NO', 'autocomplete': str(error)}, str(error), 'Check the workflow log for more information.')
 
 def alfred_error(error):
     alfred.write(alfred.xml([ErrorItem(error)]))
@@ -70,8 +78,8 @@ def copy_db(name, profile):
     db_file = os.path.join(os.path.expanduser(profile), name)
     try:
         shutil.copy(db_file, cache)
-    except:
-        raise IOError(u'Unable to copy Google Chrome history database from {}'.format(db_file))
+    except Exception:
+        raise IOError('Unable to copy Google Chrome history database from {}'.format(db_file))
 
     return cache
 
@@ -79,8 +87,8 @@ def history_db(profile, favicons=True):
     history = copy_db(HISTORY_DB, profile)
     db = sqlite3.connect(history)
     if favicons:
-        favicons = copy_db(FAVICONS_DB, profile)
-        db.cursor().execute('ATTACH DATABASE ? AS favicons', (favicons,)).close()
+        favicons_db = copy_db(FAVICONS_DB, profile)
+        db.cursor().execute('ATTACH DATABASE ? AS favicons', (favicons_db,)).close()
     return db
 
 def cache_favicon(image_data, uid, last_updated):
@@ -89,10 +97,9 @@ def cache_favicon(image_data, uid, last_updated):
         os.makedirs(cache_dir)
     icon_file = os.path.join(cache_dir, str(uid))
     if not os.path.isfile(icon_file) or last_updated > os.path.getmtime(icon_file):
-        with open(icon_file, 'w') as f:
+        with open(icon_file, 'wb') as f:
             f.write(image_data)
         os.utime(icon_file, (time.time(), last_updated))
-
     return (icon_file, {'type': 'png'})
 
 # Chrome measures time in microseconds since the Windows epoch (1601/1/1)
@@ -100,28 +107,15 @@ def cache_favicon(image_data, uid, last_updated):
 def convert_chrometime(chrometime):
     return (chrometime * MICROSECS_PER_SEC) - SECONDS_BETWEEN_UNIX_AND_WINDOWS_EPOCH
 
-def get_matching_rows(favicon_select, favicon_join, q):
-    words = filter(lambda word: len(word.strip()) > 0, q.split())
-    criterion = " AND ".join(["(urls.title LIKE ? OR urls.url LIKE ?)" for word in words])
-
-    query = u"""
-SELECT urls.id, urls.title, urls.url {favicon_select}
-    FROM urls
-    {favicon_join}
-    WHERE ({where_clause})
-    ORDER BY visit_count DESC, typed_count DESC, last_visit_time DESC
-""".format(favicon_select=favicon_select, favicon_join=favicon_join, where_clause=criterion)
-    word_tuple = tuple([u'%{}%'.format(word) for word in words for i in range(2)])
-    return db.execute(query, word_tuple)
-
 def history_results(db, query, favicons=True):
+    q = f'%{query}%'
     if favicons:
         favicon_select = FAVICON_SELECT
         favicon_join = FAVICON_JOIN
     else:
         favicon_select = ''
         favicon_join = ''
-    for row in get_matching_rows(favicon_select, favicon_join, query):
+    for row in db.execute(HISTORY_QUERY.format(favicon_select=favicon_select, favicon_join=favicon_join), (q, q,)):
         if favicons:
             (uid, title, url, image_data, image_last_updated) = row
             icon = cache_favicon(image_data, uid, convert_chrometime(image_last_updated)) if image_data and image_last_updated else None
@@ -129,14 +123,14 @@ def history_results(db, query, favicons=True):
             (uid, title, url) = row
             icon = None
 
-        yield alfred.Item({u'uid': alfred.uid(uid), u'arg': url, u'autocomplete': url}, title or url, url, icon)
+        yield alfred.Item({'uid': alfred.uid(uid), 'arg': url, 'autocomplete': url}, title or url, url, icon)
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Alfred Chrome History {}'.format(__version__))
     favicons = arguments.get('--no-favicons') is False
 
-    profile = unicode(arguments.get('PROFILE'), encoding='utf-8', errors='ignore')
-    query = unicode(arguments.get('QUERY'), encoding='utf-8', errors='ignore')
+    profile = str(arguments.get('PROFILE'))
+    query = str(arguments.get('QUERY'))
 
     try:
         db = history_db(profile, favicons=favicons)
